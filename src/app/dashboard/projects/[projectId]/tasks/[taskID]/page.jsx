8fce9f093c26357f977c966ad6233c88521b93b0
Surendra Singh Kamboj <surendra.singh.kamboj@hotmail.com>
2024-03-18 08:18:42 +0530

    pages creted for task and projects

---
+++ src/app/dashboard/projects/[projectId]/tasks/[taskID]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskById = () => {
+  return (
+    <div>TaskById</div>
+  )
+}
+
+export default TaskById
---
+++ src/app/dashboard/projects/create-task/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateNewTask = () => {
+  return (
+    <div>CreateNewTask</div>
+  )
+}
+
+export default CreateNewTask
