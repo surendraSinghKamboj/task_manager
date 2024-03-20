e6e8d35b9bf2e6c9c71286b425ae2293e1753e70
Surendra Singh Kamboj <surendra.singh.kamboj@hotmail.com>
2024-03-20 10:45:48 +0530

    changes made

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,9 +5,9 @@ import React from "react";
 const Page = async () => {
   const data = await fetchUser();
   return (
-    <div className="w-full">
+    <div className="w-full p-4">
       <p>You have {data.projectsNumber} Projects.</p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-2">
         {data.projects &&
           data.projects.map((project) => (
             <div key={project._id}>
--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-const ProjectCard = ({ project }) => {
+const ProjectCard = ({ project:{_id,projectName,techStack,appType,gitLink,uiLink,createdBy:{name},createdAt} }) => {
   //--------------------------------------------------------- dataType-------------------------------------------------------------
   /**
    *                                               {
@@ -22,7 +22,17 @@ const ProjectCard = ({ project }) => {
    *                                               }
    */
   //---------------------------------------------------------------------------------------------------------------------------------
-  return <div></div>;
+  return <div className="w-96 h-96 relative hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition-all duration-500 flex flex-col rounded-lg">
+      <h3 className="text-center mt-2">{projectName}</h3>
+      <div className="w-full text-center bg-primary-500 cursor-pointer text-white mt-2">{appType}</div>
+      <div className="flex flex-wrap">{techStack && techStack.map((tech,index)=><div key={index} className="bg-gray-400 text-white m-2 px-2 rounded-full cursor-pointer py-1">{tech}</div>)}</div>
+      <div className="absolute bottom-2 left-2 text-gray-400">Created By: {name}</div>
+      <div className="gird gird-cols-3 w-full absolute bottom-12 px-2">
+        <a href={gitLink} target="_blank"><button className="w-24 bg-primary-500 hover:bg-primary-600 rounded-md text-white py-1 px-2">Github</button></a>
+        <a href={uiLink} target="_blank"><button className="w-24 bg-primary-500 hover:bg-primary-600 rounded-md text-white py-1 px-2 mx-4">Ui</button></a>
+        <Link href={`/dashboard/projects/${_id}`}><button className="w-24 bg-primary-500 hover:bg-primary-600 rounded-md text-white py-1 px-2">Open</button></Link>
+      </div>
+  </div>;
 };
 
 export default ProjectCard;
